b0e651db82568b50e3660fa5d9ed797df619eac5
Sujith46 <sujithsuresh246@gmail.com>
2022-02-25 22:47:53 +0530

    fix(website): form dashboard

---
+++ pages/dataRequired.tsx
@@ -0,0 +1,37 @@
+import { NextPage } from "next";
+import {
+    Stack,
+    Button,
+    FormControl,
+    FormLabel,
+    Input,
+    InputGroup,
+    InputRightElement,
+    Select,
+} from "@chakra-ui/react";
+
+const DataRequired: NextPage = () => {
+    return (
+        <div className="form-data-container">
+            <Stack maxWidth={600} margin="auto" spacing={5} marginTop={5} p={4}>
+                <FormControl>
+                    <FormLabel>Add title for your article</FormLabel>
+                    <Input isRequired type="text" mb={3}>
+                    </Input>
+                    <FormLabel>Keyword</FormLabel>
+                    <Input isRequired type="text" mb={3}></Input>
+                    <FormLabel>Platform</FormLabel>
+                    <Select placeholder='Select platform' variant="filled">
+                        <option value='Google'>Google</option>
+                        <option value='Facebook'>Facebook</option>
+                        <option value='Instagram'>Instagram</option>
+                        <option value='Linkedin'>LinkedIn</option>
+                        <option value='Snapchat'>Snapchat</option>
+                    </Select>
+                </FormControl>
+            </Stack>
+        </div>
+    );
+};
+
+export default DataRequired;

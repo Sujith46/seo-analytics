460f93746d943a9713da98db1cd66f4e3f9c1dcb
Sujith46 <sujithsuresh246@gmail.com>
2022-02-26 17:34:57 +0530

    feat(webiste): attributes form field

--- pages/dataRequired.tsx
+++ pages/dataRequired.tsx
@@ -8,12 +8,14 @@ import {
     InputGroup,
     InputRightElement,
     Select,
+    Heading,
 } from "@chakra-ui/react";
 
 const DataRequired: NextPage = () => {
     return (
         <div className="form-data-container">
             <Stack maxWidth={600} margin="auto" spacing={5} marginTop={5} p={4}>
+                <Heading as="h2" size='md' mb={8}>Please add your article attributes for maximum seo</Heading>
                 <FormControl>
                     <FormLabel>Add title for your article</FormLabel>
                     <Input isRequired type="text" mb={3}>
@@ -21,13 +23,14 @@ const DataRequired: NextPage = () => {
                     <FormLabel>Keyword</FormLabel>
                     <Input isRequired type="text" mb={3}></Input>
                     <FormLabel>Platform</FormLabel>
-                    <Select placeholder='Select platform' variant="filled">
+                    <Select placeholder='Select platform' variant="outline"  mb={16}>
                         <option value='Google'>Google</option>
                         <option value='Facebook'>Facebook</option>
                         <option value='Instagram'>Instagram</option>
                         <option value='Linkedin'>LinkedIn</option>
                         <option value='Snapchat'>Snapchat</option>
                     </Select>
+                    <Button bg="blue.700" color="white" width="100%">Submit attributes</Button>
                 </FormControl>
             </Stack>
         </div>

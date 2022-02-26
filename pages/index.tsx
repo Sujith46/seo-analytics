4e2d18a005dbbd37414410164118378bb506bbdd
Sujith46 <sujithsuresh246@gmail.com>
2022-02-26 17:35:12 +0530

    feat(webiste): hero section form field

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,12 +5,13 @@ import {
   Button, 
   Flex, 
   Heading, 
-  Input, 
+  Input,
+  Image,
   Switch, 
+  Text,
   useColorMode, 
-  useColorModeValue 
+  useColorModeValue
 } from '@chakra-ui/react'
-
 
 const Home: NextPage = () => {
   const router = useRouter();
@@ -18,12 +19,15 @@ const Home: NextPage = () => {
   const { toggleColorMode } = useColorMode();
   const formBackground = useColorModeValue("gray.100","gray.700");
   return (
-    <Flex height="100vh" alignItems="center" justifyContent="center">
-      <Flex direction="column" background={formBackground} p="12" rounded="6">
-        <Heading mb={6}>Welcome!</Heading>
-        <Input placeholder='reachsujith@gmail.com' variant="filled" mb={3} type="email" />
-        <Input placeholder='********' variant="filled" mb="6" type="password"/>
-        <Button colorScheme="teal" mb={3} onClick={() => router.push('/dataRequired')}>Log in</Button>
+    <Flex height="100vh" alignItems="center" justifyContent="center" gap={12}>
+      <Image src="/hero.jpg" alt='hero-image' maxWidth="500px"/>
+      <Flex direction="column" p="12" rounded="6" width="25%" max-width="600px">
+        <Heading mb={6}>Lets get started!</Heading>
+        <Input placeholder='reachsujith@gmail.com' variant="outline" mb={3} type="email" />
+        <Input placeholder='********' variant="outline" mb="12" type="password"/>
+        <Button bg="blue.700" color="white" mb={8} onClick={() => router.push('/dataRequired')}>Log in</Button>
+        <Text fontSize='md' textAlign="center" mb={8}>or</Text>
+        <Button mb="3">Continue with google</Button>
         <Switch onChange={toggleColorMode}/>
       </Flex>
     </Flex>
